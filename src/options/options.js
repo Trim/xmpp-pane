908e349bb12f26f2801c2b01ea9567428e0d0efa
Adrien Dorsaz <adrien@adorsaz.ch>
2017-11-29 01:08:07 +0100

    options: first implementation of setting page (mono-account)

---
+++ src/options/options.js
@@ -0,0 +1,43 @@
+function saveOptions(e) {
+    e.preventDefault();
+
+    function storePassword(hash) {
+        browser.storage.local.set({
+            password: hash
+            });
+    }
+
+    function digestError(error) {
+        console.log('options: error on computing password hash: ' + error);
+    }
+
+    // Save new password if required
+    let rawPassword = document.querySelector("#password").value;
+    if (rawPassword) {
+        let bufferPassword = new TextEncoder("utf-8").encode(rawPassword);
+        crypto.subtle.digest('SHA-512', bufferPassword).then(storePassword, digestError);
+    }
+
+    // Save jid
+    browser.storage.local.set({
+        jid: document.querySelector("#jid").value
+        });
+}
+
+function restoreOptions() {
+
+    function setJidInput(result) {
+        document.querySelector("#jid").value = result.jid || "";
+    }
+
+    function onError(error) {
+        console.log('options: error:' + error);
+    }
+
+    let getJid = browser.storage.local.get("jid");
+    getJid.then(setJidInput, onError);
+}
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
+
