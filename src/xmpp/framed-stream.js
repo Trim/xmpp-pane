3b078bb230e2fe6380e1f86425affb4c113754ec
Adrien Dorsaz <adrien@adorsaz.ch>
2017-12-08 23:54:24 +0100

    framed-stream: add documentation and try to call parent initiate method when opening.

--- src/xmpp/framed-stream.js
+++ src/xmpp/framed-stream.js
@@ -11,20 +11,36 @@ class FramedStream extends Stream {
     /*
      * <open /> is used to open a stream header after WebSocket initialization
      *
+     * As websocket has to send complete XML document, this replace the opening
+     * part of <stream:stream> tag from RFC-6120 which is closed only on end of XMPP session.
+     *
      * [RFC-7395] Section-3.4
      */
     open() {
-        return new Promise((resolve, reject) => {
-            let open = this.dom.createElementNS(Constants.NS_XMPP_FRAMING, "open");
-            open.setAttribute("to", this.to);
-            open.setAttribute("version", Constants.XMPP_VERSION);
+        return super.initiate()
+            .then((streamElement) => {
+                return new Promise((resolve, reject) => {
+                    let open = this.dom.createElementNS(Constants.NS_XMPP_FRAMING, "open");
+                    open.setAttribute("from", this.from);
+                    open.setAttribute("to", this.to);
+                    if (this.id) {
+                        open.setAttribute("id", this.id);
+                    }
+                    if (this.xmllang) {
+                        open.setAttribute("xml:lang", this.xmllang);
+                    }
+                    open.setAttribute("version", Constants.XMPP_VERSION);
 
-            resolve(this.xmlSerializer.serializeToString(open));
-        });
+                    resolve(this.xmlSerializer.serializeToString(open));
+                });
+            });
     }
 
     /*
      * <close /> is used to close a stream header before WebSocket close
+     *
+     * As websocket has to send complete XML document, this replace the closing part
+     * of </stream:stream> tag from RFC-6120 which closes the current XMPP session.
      *
      * [RFC-7395] Section-3.6
      */
